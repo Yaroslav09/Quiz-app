6c38df1a977c96e0e78deae4d5cfa0a0644a54ce
Yaroslav Bozhak <bozhakyaroslav@gmail.com>
2024-01-18 08:49:14 +0000

    fixed bug

--- quiz.js
+++ quiz.js
@@ -41,7 +41,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score);
 
-        return window.location.assign(href="../../result.html");
+        return window.location.assign("./result.html");
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
